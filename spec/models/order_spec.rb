e7d4c75f1fc3c42a5af7e638e4f41ba06b1f9255
Kota Ishida <kota.ishida@di-v.co.jp>
2024-05-08 21:55:05 +0900

    テストコード実装

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @order = FactoryBot.build(:order)
+  end
+
+  context '内容に問題ない場合' do
+    it "priceがあれば保存ができること" do
+      #学習用コメント：be_validは`valid?`を呼び出し、trueを返すかどうかを検証するメソッド。`true`を返す場合、そのオブジェクトは「有効」と判定され、データベースに保存できる状態
+      expect(@order).to be_valid
+    end
+  end
+
+  context '内容に問題がある場合' do
+    it "priceが空では保存ができないこと" do
+      @order.price = nil
+      @order.valid?
+      expect(@order.errors.full_messages).to include("Price can't be blank")
+    end
+  end
 end
