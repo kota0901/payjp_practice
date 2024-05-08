7447240f9cebac5d7de01d1a75358a31d38dce13
Kota Ishida <kota.ishida@di-v.co.jp>
2024-05-09 03:04:21 +0900

    テストコード実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
+  #ordersテーブルにtokenカラムは存在しないが、「attr_accessor」により使用することができる
   attr_accessor :token
   validates :price, presence: true
+  validates :token, presence: true
 end
